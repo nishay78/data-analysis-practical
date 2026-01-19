1c5f045d57277b6a896b81474362bd4b8e267d8a
Nisha Yadav <Nishay78@gmail.com>
2026-01-19 19:35:23 +0530

    Initial commit

---
+++ CodeFile.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Load CSV file
+csv_data = pd.read_csv("C:\\Users\\ACER\\OneDrive\\Desktop\\MyProject\\Dataset\\Students.csv")
+
+# Load Excel file
+#excel_data = pd.read_excel("data/customers.xlsx")
+
+print("CSV Data Preview:")
+print(csv_data.head())
+
+#print("\nExcel Data Preview:")
+#print(excel_data.head())
